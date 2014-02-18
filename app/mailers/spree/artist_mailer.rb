d7c4e30592f9186ba2d5943174eef45362af50a1
Emmanuel Jean Kamasah <jeanlinux5@gmail.com>
2014-02-18 18:11:28 +0000

    Fixed artist signup error

--- app/mailers/spree/artist_mailer.rb
+++ app/mailers/spree/artist_mailer.rb
@@ -2,8 +2,9 @@ superclass = defined?(Spree::BaseMailer) ? Spree::BaseMailer : ActionMailer::Bas
 
 class Spree::ArtistMailer < superclass
   def artist_signup(user)
-
+    # @user = User.find(id)
+    artist = user
     mail(:to => "nene.joetee@gmail.com", :from => "robot@madeinafrica.net",
-         :subject => "New Artist Account Created!")
+         :subject => "New Artist Account Created!(Email: #{user.email} : Tel: #{user.request_telephone}: Email: #{user.email})")
   end
 end
